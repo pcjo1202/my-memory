26e2ede6b1417309cd099be62d363606ff722a88
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-11-17 23:28:08 +0900

    Add header component UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import styles from './app.module.css'
+import Main from './components/main/main'
 
 function App () {
-  return <div className='App' />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = props => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_holder}>
+        <h1 className={styles.logo}>
+          <a href='' className={styles.logo_a}>
+            <img src='/image/logo_transparent.png' alt='하루를 기록' />
+          </a>
+        </h1>
+        <nav className={styles.btn_nav}>
+          <a href='#' className={styles.nav}>
+            <div className={styles.hamburger} />
+          </a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
