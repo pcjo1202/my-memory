60049d9b4cfbd409bdbe7f81bb2ef8ddfa2dacfe
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-12-03 21:24:48 +0900

    Make LodingSpinner component

---
+++ src/components/loading_spinner/loading_spinner.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './loading_spinner.module.css'
+const LoadingSpinner = () => {
+  return <div className={styles.loading_spinner} />
+}
+
+export default LoadingSpinner
