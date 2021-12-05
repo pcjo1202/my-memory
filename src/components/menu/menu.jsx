2cfd09f424a34eb92608e6792c7e038b5c093920
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-12-05 19:06:16 +0900

    Add menu mediaQuery in mobile

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './menu.module.css'
 
 const Menu = () => {
+  const wrapperRef = useRef()
+  const menuRef = useRef()
+
+  const increaseMenu = () => {
+    wrapperRef.current.classList.toggle(`${styles.increase}`)
+    menuRef.current.classList.toggle(`${styles.active}`)
+    console.log(menuRef.current)
+    console.log(wrapperRef.current)
+  }
+
   return (
-    <nav className={styles.menu_wrapper}>
-      <div className={styles.menu_icon}>
+    <nav ref={wrapperRef} className={styles.menu_wrapper}>
+      <div ref={menuRef} className={styles.menu_icon} onClick={increaseMenu}>
         <div className={styles.menu_hamburger} />
       </div>
       <ul className={styles.sub_icon_list}>
