914d4ba69126c8f1d3a31fc8ad419dd2ab6408d6
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-12-05 22:11:14 +0900

    Make header_diary component

---
+++ src/components/header_diary/header_diary.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './header_diary.module.css'
+
+const HeaderDiary = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src='/image/logo_transparent.png' alt='logo' />
+      </div>
+      <div className={styles.title}>
+        <p>모든노트</p>
+      </div>
+    </header>
+  )
+}
+
+export default HeaderDiary
