7b00e13f89e6157f0cbf6d76608f04fd5cf1e603
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-12-05 20:39:28 +0900

    Modify

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -20,12 +20,15 @@ const Menu = () => {
       <ul className={styles.sub_icon_list}>
         <li className={styles.sub_icon}>
           <i className={`${styles.home_icon} fas fa-home`} />
+          <span className={styles.icon_text}>Home</span>
         </li>
         <li className={styles.sub_icon}>
           <i className={`${styles.bookmark_icon} far fa-bookmark`} />
+          <span className={styles.icon_text}>Bookmark</span>
         </li>
         <li className={styles.sub_icon}>
           <i className={`${styles.folder_icon} far fa-folder`} />
+          <span className={styles.icon_text}>Folder</span>
         </li>
       </ul>
       <div className={`${styles.setting_btn} ${styles.sub_icon}`}>
