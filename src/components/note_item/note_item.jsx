a09c974608fbdf95b9f412e937a9e884080e569a
pcjo1202 <73429928+pcjo1202@users.noreply.github.com>
2021-12-10 18:23:36 +0900

    Modify note UI

--- src/components/note_item/note_item.jsx
+++ src/components/note_item/note_item.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import styles from './note_item.module.css'
 const NoteItem = ({ memo }) => {
-  const { thumnail, title } = memo
+  const { hashtag, title, date } = memo
   return (
     <li className={styles.note_item}>
-      <div className={styles.preview}>
-        {thumnail}
-      </div>
-      <p className={styles.title}>
+      <div className={styles.title}>
         {title}
+      </div>
+      <p className={styles.date}>
+        {date}
+      </p>
+      <p className={styles.hashtag}>
+        {hashtag}
       </p>
     </li>
   )
